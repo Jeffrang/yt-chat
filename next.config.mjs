cb4ded20fb64974861065b1d722b0322ca7dad38
Jefree Sujit <e.jefree110@gmail.com>
2024-10-10 18:23:00 +0000

    Support searching of youtube video instead of just url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.youtube.com'],
+    domains: [
+      'img.youtube.com',
+      'i.ytimg.com',
+    ],
   },
 };
 
