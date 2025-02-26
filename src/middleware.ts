3011b694eccdde199b51d0b4b4e38362edd86fb4
Jefree Sujit <e.jefree110@gmail.com>
2025-02-26 14:42:28 +0000

    restrict all routes except keep-alive

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,6 +11,10 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/api/chat/(.*)',
+    '/api/chats/(.*)',
+    '/api/create-chat/(.*)',
+    '/api/search/(.*)',
+    '/trpc/(.*)',
   ],
 }
