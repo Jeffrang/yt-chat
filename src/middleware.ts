a5a483cfc3f0bbacb8e4a52ba45114d75bde0564
Jefree Sujit <e.jefree110@gmail.com>
2025-02-26 13:18:11 +0000

    update middleware conditional check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export default clerkMiddleware()
+// Create custom middleware
+export default function middleware(req: NextRequest) {
+  // Skip auth for /api/keep-alive
+  if (req.nextUrl.pathname === '/api/keep-alive') {
+    return NextResponse.next()
+  }
+
+  // Apply clerk auth for all other routes
+  return clerkMiddleware(req)
+}
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes except for /api/keep-alive
-    '/(api|trpc)(?!/keep-alive).*',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 }
